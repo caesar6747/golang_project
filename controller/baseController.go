f46b14a37276b7f578f72851f2bb24f368afbcdc
caesar6747 <caesarhnc100@gmail.com>
2023-12-10 02:39:40 +0700

    add module controller

--- controller/baseController.go
+++ controller/baseController.go
@@ -2,6 +2,6 @@ package controller
 
 import "net/http"
 
-func testController(w http.ResponseWriter, r *http.Request) {
+func TestController(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("this is test page"))
 }
